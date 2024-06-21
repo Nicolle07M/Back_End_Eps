ea24a2d275a7947e3104b544562f4f9d1cd80d59
Nicolle <marqueznicolle421@gmail.com>
2024-06-20 21:24:16 -0500

    feat(BackEnd): creando modelos

--- database/migrations/2024_06_21_020439_create_doctors_table.php
+++ database/migrations/2024_06_21_020439_create_doctors_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('doctores', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('age');
+            $table->unsignedBigInteger('specialty_id');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
+
+            $table->foreign('specialty_id')->references('id')->on('specialties');
         });
     }
 
